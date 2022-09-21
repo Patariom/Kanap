d0151669808aa588aa737b9d221250152194fcf0
Marion Perrié <marion.perrie@gmail.com>
2022-09-21 12:17:01 +0200

    refactor[script]: modify function display products
    Create 2 functions instead of the previous 1 : the first is to fetch
    products, the second is to display them.

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,8 @@
+displayProducts();
+
 /** 
- * Fetch and display products on homepage
+ * Fetch products on homepage
+ * (from local API)
  */
 async function fetchProducts() {
     //Fetch products
@@ -9,43 +12,58 @@ async function fetchProducts() {
     //Check if products are fetched
     if (!fetchRequest.ok) {
         //Products are not fetched, display error message
-        alert("Un problème est survenu, merci de réessayer ultérieurement !");
+        console.log("Un problème est survenu, merci de réessayer ultérieurement !");
     } else {
-        //Products are fetched, proceed
-        let listProducts = await fetchRequest.json();
-        console.log("Les produits ont bien été récupérés.");
+        //Products are fetched, stock them in json
+        var listProducts = await fetchRequest.json();
+        console.log("Les produits ont bien été récupérés !");
+        return listProducts;        
+    }
 
-        //Display products
-        for (let i in listProducts) {
-
-            const items = document.querySelector("#items");
-
-            //Create contenair a
-            const productLink       = document.createElement("a");
-            // productLink = listProducts[i]._id;
-
-            items.appendChild(productLink);
-            //Create contenair article
-            const productArticle    = document.createElement("article");
-            productLink.appendChild(productArticle);
-
-            //Create product image
-            const productImage      = document.createElement("img");
-            productArticle.appendChild(productImage);
-            productImage.src = listProducts[i].imageUrl;
-            productImage.alt = listProducts[i].altTxt;
-
-            //Create product title
-            const productTitle      = document.createElement("h3");
-            productArticle.appendChild(productTitle);
-            productTitle.textContent = listProducts[i].name;
-
-            //Create product description
-            const productDescription= document.createElement("p");
-            productArticle.appendChild(productDescription);
-            productDescription.textContent = listProducts[i].description;
-        };
-    };
 }
 
-fetchProducts();
+
+/** 
+ * Create and display products on homepage
+ * (from local API)
+ */
+async function displayProducts() {
+    //Fetch data
+    let product = await fetchProducts();
+
+    //Create each card
+    for (let i in product) {
+
+        const items = document.querySelector("#items");
+
+        //Create contenair a
+        const productLink = document.createElement("a");
+        // productLink.href = new URL(document.location);
+        // // url.searchParams.set('id', [listProducts[i]._id]);
+        items.appendChild(productLink);
+
+        //Create contenair article
+        const productArticle = document.createElement("article");
+        productLink.appendChild(productArticle);
+
+        //Create product image
+        const productImage = document.createElement("img");
+        productArticle.appendChild(productImage);
+        productImage.src = product[i].imageUrl;
+        productImage.alt = product[i].altTxt;
+
+        //Create product title
+        const productTitle = document.createElement("h3");
+        productArticle.appendChild(productTitle);
+        productTitle.textContent = product[i].name;
+
+        //Create product description
+        const productDescription = document.createElement("p");
+        productArticle.appendChild(productDescription);
+        productDescription.textContent = product[i].description;
+
+    };
+
+    console.log("Les produits ont bien été créés !")
+
+};
