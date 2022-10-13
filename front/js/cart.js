bbcfc8d261927fbd0636dcba2a773877894e0e57
Marion Perrié <marion.perrie@gmail.com>
2022-10-13 17:21:23 +0200

    refactor[cart.js]: transform displayCart in function
    Add a function to the displayCart loop
    Add a if/else condition to change cart page if cart is empty

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,11 +4,6 @@
 let cart = JSON.parse(localStorage.getItem("cart"));
 
 
-//Select cart container
-let cartItems = document.querySelector("#cart__items");
-
-
-
 /** 
  * Fetch right sofa according to the ID
  */
@@ -33,104 +28,123 @@ async function fetchSofa(idSofa) {
 
 }
 
-
-
 /** 
- * Create a looop that will display cart items from Local Storage
+ * Create a function and a looop that will display cart items from Local Storage
  * and fetch others informations concerning these items from API
+ * The function will also display a message and hide order form if cart is empty
  *
  */
 
-cart.forEach(async i => {
+function displayCart() {
 
+    //Select cart container
+    let cartItems = document.querySelector("#cart__items");
 
-    //Select and Fetch Sofa according to ID
-    let idArticle = i.id;
-    let colorArticle = i.color;
-    let article = await fetchSofa(idArticle);
+    if (cart) {
+        cart.forEach(async i => {
+            //Select and Fetch Sofa according to ID
+            let idArticle = i.id;
+            let colorArticle = i.color;
+            let article = await fetchSofa(idArticle);
 
-    //Create an article in HTML to display the item
-    let cartArticle = document.createElement("article");
-    cartArticle.className = "cart__item";
-    cartArticle.dataset.id = idArticle;
-    cartArticle.dataset.color = colorArticle;
-    cartItems.appendChild(cartArticle);
+            //Create an article in HTML to display the item
+            let cartArticle = document.createElement("article");
+            cartArticle.className = "cart__item";
+            cartArticle.dataset.id = idArticle;
+            cartArticle.dataset.color = colorArticle;
+            cartItems.appendChild(cartArticle);
 
-    //Create a div which will be used to display the visual content
-    let articleIMGContainer = document.createElement("div");
-    cartArticle.appendChild(articleIMGContainer);
-    articleIMGContainer.className = "cart__item__img";
+            //Create a div which will be used to display the visual content
+            let articleIMGContainer = document.createElement("div");
+            cartArticle.appendChild(articleIMGContainer);
+            articleIMGContainer.className = "cart__item__img";
 
-    //Create and fetch the image of the article
-    let articleIMGPhoto = document.createElement("img");
-    articleIMGContainer.appendChild(articleIMGPhoto);
-    articleIMGPhoto.src = article.imageUrl;
-    articleIMGPhoto.alt = article.altTxt;
+            //Create and fetch the image of the article
+            let articleIMGPhoto = document.createElement("img");
+            articleIMGContainer.appendChild(articleIMGPhoto);
+            articleIMGPhoto.src = article.imageUrl;
+            articleIMGPhoto.alt = article.altTxt;
 
-    //Create a div which will be used to display the text content (description and settings)
-    let articleInfoContainer = document.createElement("div");
-    cartArticle.append(articleInfoContainer);
-    articleInfoContainer.className = "cart__item__content";
+            //Create a div which will be used to display the text content (description and settings)
+            let articleInfoContainer = document.createElement("div");
+            cartArticle.append(articleInfoContainer);
+            articleInfoContainer.className = "cart__item__content";
 
-    //Create a div which will be used to display the description
-    let articleInfoDescription = document.createElement("div");
-    articleInfoContainer.appendChild(articleInfoDescription);
-    articleInfoDescription.className = "cart__item__content__description";
+            //Create a div which will be used to display the description
+            let articleInfoDescription = document.createElement("div");
+            articleInfoContainer.appendChild(articleInfoDescription);
+            articleInfoDescription.className = "cart__item__content__description";
 
-    //Create and Fetch the article name from API
-    let articleName = document.createElement("h2");
-    articleInfoDescription.appendChild(articleName);
-    articleName.textContent = article.name;
+            //Create and Fetch the article name from API
+            let articleName = document.createElement("h2");
+            articleInfoDescription.appendChild(articleName);
+            articleName.textContent = article.name;
 
-    //Create and Fetch the article color from LS
-    let articleColor = document.createElement("p");
-    articleInfoDescription.appendChild(articleColor);
-    articleColor.textContent = i.color;
+            //Create and Fetch the article color from LS
+            let articleColor = document.createElement("p");
+            articleInfoDescription.appendChild(articleColor);
+            articleColor.textContent = i.color;
 
-    //Create and Fetch the article price from API
-    let articlePrice = document.createElement("p");
-    articleInfoDescription.appendChild(articlePrice);
-    articlePrice.textContent = article.price + " €";
+            //Create and Fetch the article price from API
+            let articlePrice = document.createElement("p");
+            articleInfoDescription.appendChild(articlePrice);
+            articlePrice.textContent = article.price + " €";
 
-    //Create a div which will be used to display the settings
-    let articleInfoSettings = document.createElement("div");
-    articleInfoContainer.appendChild(articleInfoSettings);
-    articleInfoSettings.className = "cart__item__content__settings";
+            //Create a div which will be used to display the settings
+            let articleInfoSettings = document.createElement("div");
+            articleInfoContainer.appendChild(articleInfoSettings);
+            articleInfoSettings.className = "cart__item__content__settings";
 
-    //Create a div which will be used to display the quantity setings and number
-    let articleQuantity = document.createElement("div");
-    articleInfoSettings.appendChild(articleQuantity);
-    articleQuantity.className = "cart__item__content__settings__quantity";
+            //Create a div which will be used to display the quantity setings and number
+            let articleQuantity = document.createElement("div");
+            articleInfoSettings.appendChild(articleQuantity);
+            articleQuantity.className = "cart__item__content__settings__quantity";
 
-    //Create the text "Qté"
-    let articleQuantityTxt = document.createElement("p");
-    articleQuantity.appendChild(articleQuantityTxt);
-    articleQuantityTxt.textContent = "Qté :";
+            //Create the text "Qté"
+            let articleQuantityTxt = document.createElement("p");
+            articleQuantity.appendChild(articleQuantityTxt);
+            articleQuantityTxt.textContent = "Qté :";
 
-    //Create the input to display and change quantity
-    let articleQuantityInput = document.createElement("input");
-    articleQuantity.appendChild(articleQuantityInput);
-    articleQuantityInput.className = "itemQuantity";
-    articleQuantityInput.type = "number";
-    articleQuantityInput.name = "itemQuantity";
-    articleQuantityInput.min = "1";
-    articleQuantityInput.max = "100";
+            //Create the input to display and change quantity
+            let articleQuantityInput = document.createElement("input");
+            articleQuantity.appendChild(articleQuantityInput);
+            articleQuantityInput.className = "itemQuantity";
+            articleQuantityInput.type = "number";
+            articleQuantityInput.name = "itemQuantity";
+            articleQuantityInput.min = "1";
+            articleQuantityInput.max = "100";
 
-    //Create and Fetch the article quantity from Local Storage
-    articleQuantityInput.value = parseInt(i.quantity);
+            //Create and Fetch the article quantity from Local Storage
+            articleQuantityInput.value = parseInt(i.quantity);
 
-    //Create a div which will be used to display the delete option
-    let articleDeleteContainer = document.createElement("div");
-    articleInfoSettings.appendChild(articleDeleteContainer);
-    articleDeleteContainer.className = "cart__item__content__settings__delete";
+            //Create a div which will be used to display the delete option
+            let articleDeleteContainer = document.createElement("div");
+            articleInfoSettings.appendChild(articleDeleteContainer);
+            articleDeleteContainer.className = "cart__item__content__settings__delete";
 
-    //Create the delete text
-    let articleDeleteOption = document.createElement("p");
-    articleDeleteContainer.appendChild(articleDeleteOption);
-    articleDeleteOption.className = "deleteItem";
-    articleDeleteOption.textContent = "Supprimer";
-});
+            //Create the delete text
+            let articleDeleteOption = document.createElement("p");
+            articleDeleteContainer.appendChild(articleDeleteOption);
+            articleDeleteOption.className = "deleteItem";
+            articleDeleteOption.textContent = "Supprimer";
+        })
 
+    } else {
+        //Display a message saying that cart is empty
+        let emptyBasket = document.querySelector("h1")
+        emptyBasket.textContent = "Votre panier est vide :(";
+
+        //hide quantity and price items
+        let infoPrice = document.querySelector(".cart__price");
+        infoPrice.textContent = "";
+
+        //hide order form
+        let orderForm = document.querySelector(".cart__order");
+        orderForm.textContent = "";
+    }
+}
+
+displayCart();
 
 
 //-------------------------------------- DISPLAY TOTAL PRICE AND QTY --------------------------------------
@@ -141,18 +155,21 @@ cart.forEach(async i => {
 let totalQuantity = document.querySelector("#totalQuantity");
 
 //Calculate quantity with reduce function
-const totalArticles = cart.reduce((a, b) => a + b.quantity, 0)
+const totalArticles = cart.reduce((a, b) => a + b.quantity, 0);
 
 //Display number 
 totalQuantity.textContent = totalArticles;
 
 //Put total quantity number next to the cart icon
+//Should be put on a separate JS file to work on all pages
 function displayQtyInNavBar() {
-let navBar = document.querySelectorAll("nav a li");
-let cartIcon = navBar[1];
-let totalInCartIcon = document.createElement("span");
-cartIcon.append(totalInCartIcon);
-totalInCartIcon.textContent = "(" + totalArticles + ")";
+    let navBar = document.querySelectorAll("nav a li");
+    let cartIcon = navBar[1];
+    let totalInCartIcon = document.createElement("span");
+    cartIcon.append(totalInCartIcon);
+    totalInCartIcon.textContent = "(" + totalArticles + ")";
 }
 displayQtyInNavBar();
+
+
 
