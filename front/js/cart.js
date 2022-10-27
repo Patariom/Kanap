479b39c337e3814ae86eb25f1af10afb2c3f1a9b
Marion Perrié <marion.perrie@gmail.com>
2022-10-27 16:44:16 +0200

    feat[cart.js] = post data to API and redirect to confirmation page
    Create a postOrder(order) function that will post the data to API,
    get the response from the API and redirect to a confirmation page according
    to the response ID

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,8 +4,67 @@
 //Get cart and put it in a variable
 let cart = JSON.parse(localStorage.getItem("cart"));
 
-
 //-------------------------------------- END OF GLOBAL VARIABLES --------------------------------------
+
+
+//-------------------------------------- FETCH AND POST FUNCTIONS --------------------------------------
+
+/** 
+ * Fetch right sofa from API according to the ID
+ * It will be used to get all informations about sofa that weren't put in the LS 
+ */
+ async function fetchSofa(idSofa) {
+
+    //Fetch Sofa according to ID
+    const fetchSofa = await fetch(`http://localhost:3000/api/products/${idSofa}`, {
+        method: "GET",
+    })
+
+    //Check if sofa is fetched
+    if (!fetchSofa.ok) {
+
+        //Sofa is not fetched, display error message
+        console.log("Un problème est survenu");
+
+    } else {
+        //Sofa is fetched, stock it in json
+        var sofa = await fetchSofa.json();
+        return sofa;
+    }
+}
+
+
+/** 
+ * Post Order and get order confirmation ID
+ *
+ */
+ async function postOrder(order) {
+
+    const req = await fetch("http://localhost:3000/api/products/order", {
+        method: "POST", 
+        headers: {
+            "Content-Type" : "application/JSON"
+        },
+        body: JSON.stringify(order),  
+    });
+
+    //If the request returns an error
+    if(!req.ok) {
+    alert("Un problème est survenu, merci de réessayer !");
+
+    //If the request is working
+    } else {
+    console.log("La commande a bien été envoyée");
+
+    //Put the request result in a variable
+    let response =  await req.json();
+    
+    //Redirect client to confirmation page
+    document.location.href = `confirmation.html?orderid=${response.orderId}`
+    }
+}
+
+//-------------------------------------- END OF FETCH AND POST FUNCTIONS --------------------------------------
 
 
 //-------------------------------------- DISPLAY TOTAL PRICE AND QTY --------------------------------------
@@ -25,7 +84,6 @@ cartIcon.append(totalInCartIcon);
  * with reduce method
  *
  */
-
 function getTotalQuantity() {
 
     //Select total quantity container
@@ -47,11 +105,9 @@ function getTotalQuantity() {
  * For it to work on all pages, it should be put in a separate JS file and called on each page
  *
  */
-
 function displayQtyInNavBar() {
-    totalInCartIcon.textContent = "(" + getTotalQuantity() + ")"; 
+    totalInCartIcon.textContent = "(" + getTotalQuantity() + ")";
 }
-
 
 
 /** 
@@ -59,7 +115,6 @@ function displayQtyInNavBar() {
  * with reduce method
  *
  */
-
 async function getTotalPrice() {
 
     //Select total price container
@@ -69,21 +124,21 @@ async function getTotalPrice() {
     let priceArray = [];
 
     //Create a loop to calculate each item total price
-     for (let a of cart) {
+    for (let a of cart) {
         //Set the variables 
         let idArticle = a.id;
         let quantityArticle = parseInt(a.quantity);
-    
+
         //Fetch the price from  API
         let article = await fetchSofa(idArticle);
         let priceArticle = parseInt(article.price);
-    
+
         //Calculate the total price by articles
         totalPriceByArticle = priceArticle * quantityArticle;
-    
+
         //Push this number into an array
         priceArray.push(totalPriceByArticle);
-    
+
     };
 
     //Reduce the array in a sum
@@ -93,9 +148,7 @@ async function getTotalPrice() {
     totalPriceContainer.textContent = new Intl.NumberFormat('fr-FR').format(totalPriceSum);
 }
 
-
 //-------------------------------------- END OF DISPLAY TOTAL PRICE AND QTY --------------------------------------
-
 
 
 //-------------------------------------- DISPLAY PRODUCT IN CART --------------------------------------
@@ -106,36 +159,12 @@ let cartItems = document.querySelector("#cart__items");
 
 
 /** 
- * Fetch right sofa according to the ID
- */
- async function fetchSofa(idSofa) {
-
-    //Fetch Sofa according to ID
-    const fetchSofa = await fetch(`http://localhost:3000/api/products/${idSofa}`, {
-        method: "GET",
-    })
-
-    //Check if sofa is fetched
-    if (!fetchSofa.ok) {
-
-        //Sofa is not fetched, display error message
-        console.log("Un problème est survenu");
-
-    } else {
-        //Sofa is fetched, stock it in json
-        var sofa = await fetchSofa.json();
-        return sofa;
-    }
-
-}
-
-/** 
  * Create a function and a looop that will display cart items from Local Storage
  * and fetch others informations concerning these items from API
+ * 
  * The function will also display a message and hide order form if cart is empty
  *
  */
-
 displayCart();
 
 function displayCart() {
@@ -146,12 +175,12 @@ function displayCart() {
 
     if (cart) {
         cart.forEach(async i => {
-            
+
             //Select and Fetch Sofa according to ID
             let idArticle = i.id;
             let colorArticle = i.color;
             let quantityArticle = i.quantity;
-            
+
             let article = await fetchSofa(idArticle);
 
             //Create an article in HTML to display the item
@@ -195,7 +224,7 @@ function displayCart() {
             //Create and Fetch the article price from API
             let articlePrice = document.createElement("p");
             articleInfoDescription.appendChild(articlePrice);
-            articlePrice.textContent = new Intl.NumberFormat('fr-FR', {style:'currency', currency: 'EUR', maximumFractionDigits: 0}).format(article.price); 
+            articlePrice.textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR', maximumFractionDigits: 0 }).format(article.price);
 
             //Create a div which will be used to display the settings
             let articleInfoSettings = document.createElement("div");
@@ -219,10 +248,10 @@ function displayCart() {
             articleQuantityInput.type = "number";
             articleQuantityInput.name = "itemQuantity";
             articleQuantityInput.min = "1";
-            articleQuantityInput.max = "100";        
+            articleQuantityInput.max = "100";
 
             //Add the Update Quantity function Function for each Input
-            articleQuantityInput.addEventListener("change", (event)=> {
+            articleQuantityInput.addEventListener("change", (event) => {
                 updateQuantity(event)
             });
 
@@ -236,23 +265,23 @@ function displayCart() {
             articleDeleteContainer.className = "cart__item__content__settings__delete";
 
             //Create the delete text
-            let articleDeleteButton= document.createElement("p");
+            let articleDeleteButton = document.createElement("p");
             articleDeleteContainer.appendChild(articleDeleteButton);
             articleDeleteButton.className = "deleteItem";
             articleDeleteButton.textContent = "Supprimer";
 
             //Add the Delete Function for each Button
-            articleDeleteButton.addEventListener("click", (event)=> {
+            articleDeleteButton.addEventListener("click", (event) => {
                 deleteArticle(event);
             });
 
         })
 
-    getTotalQuantity();
+        getTotalQuantity();
 
-    displayQtyInNavBar();
-        
-    getTotalPrice();
+        displayQtyInNavBar();
+
+        getTotalPrice();
 
     } else {
         //Display a message saying that cart is empty
@@ -270,56 +299,52 @@ function displayCart() {
 
 }
 
-
 //-------------------------------------- END OF DISPLAY CART --------------------------------------
 
 
 //-------------------------------------- REMOVE OR UPDATE QUANTITY --------------------------------------
-
 
 /** 
  * Create a delete action to remove select item in cart
  * It's combined with an addeventlistener in displaySofa()
  *
  */
-
 function deleteArticle(event) {
 
     //Check if user did not click by mistake
-    if(confirm("Souhaitez-vous vraiment supprimer cet article ?") == true) {
+    if (confirm("Souhaitez-vous vraiment supprimer cet article ?") == true) {
 
         //Select corresponding article
-        let deletedArticle = event.target.closest("article");        
+        let deletedArticle = event.target.closest("article");
 
         //Fetch product ID and product color
         let deletedArticleID = deletedArticle.getAttribute("data-id");
-        let deletedArticleColor = deletedArticle.getAttribute("data-color"); 
-     
+        let deletedArticleColor = deletedArticle.getAttribute("data-color");
 
         //Remove the article from the DOM
         deletedArticle.remove();
-   
 
         //Use filter to keep only the items that don't have the same color and id
         cart = cart.filter(i => (i.id !== deletedArticleID) || (i.color !== deletedArticleColor));
-  
+
         //Save the cart 
         localStorage.setItem("cart", JSON.stringify(cart));
 
         //If cart is empty, remove the cart key in local storage
-        if (cart.length ==0 ) {
-            localStorage.removeItem("cart");  
+        if (cart.length == 0) {
+            localStorage.removeItem("cart");
         }
 
         //Call functions to update totals
         getTotalQuantity();
 
         displayQtyInNavBar();
-    
+
         getTotalPrice();
 
     }
 }
+
 
 /** 
  * Update quantity
@@ -328,11 +353,11 @@ function deleteArticle(event) {
 function updateQuantity(event) {
 
     //Select corresponding article
-    let modifiedArticle = event.target.closest("article");      
+    let modifiedArticle = event.target.closest("article");
 
     //Get the ID and color of the article
     let modifiedArticleId = modifiedArticle.getAttribute("data-id");
-    let modifiedArticleColor  = modifiedArticle.getAttribute("data-color");
+    let modifiedArticleColor = modifiedArticle.getAttribute("data-color");
 
     //Find in the LS the article corresponding to the ID and color of the article
     let article = cart.find(i => (i.id == modifiedArticleId) && (i.color == modifiedArticleColor));
@@ -341,18 +366,18 @@ function updateQuantity(event) {
     let articleNewQuantity = parseInt(event.target.value);
 
     //If the new quantity is between 1 and 100, update quantity in LS and call functions to calculate totals
-    if(article && (articleNewQuantity > 0 && articleNewQuantity <= 100)) {
+    if (article && (articleNewQuantity > 0 && articleNewQuantity <= 100)) {
         article.quantity = articleNewQuantity;
         localStorage.setItem("cart", JSON.stringify(cart));
-    
+
         getTotalQuantity();
-      
+
         getTotalPrice();
 
         displayQtyInNavBar();
     }
     //If new quantity is 0 and less, delete the article 
-    else if(articleNewQuantity <=0){
+    else if (articleNewQuantity <= 0) {
         deleteArticle(event);
     }
 
@@ -363,17 +388,15 @@ function updateQuantity(event) {
 
     }
 };
-    
-
-
 
 //-------------------------------------- END OF REMOVE OR UPDATE QUANTITY --------------------------------------
 
 
-
 //-------------------------------------- FORM MANAGEMENT ----------------------------------------------------------
 
+////// Variables
 let orderForm = document.querySelector(".cart__order__form");
+
 
 //Select the differents inputs of the form
 let firstName = document.querySelector("#firstName");
@@ -382,8 +405,10 @@ let address = document.querySelector("#address");
 let city = document.querySelector("#city");
 let email = document.querySelector("#email")
 
+
 //Create values for input
 let firstNameValue, lastNameValue, addressValue, cityValue, emailValue;
+
 
 //Create a set of regex rules
 let regexClassic = new RegExp(/^[a-z çàâäéèêëïîôöùûüÿæœ -]{2,90}$/, "i");
@@ -396,15 +421,15 @@ let regexEmail = new RegExp(/^[\w-\.]+@[\w-\.]+\.[\w]{2,6}$/, "i");
  *
  */
 function checkFirstName() {
-        if (firstName.value.match(regexClassic)) {
+    if (firstName.value.match(regexClassic)) {
         firstName.style.border = "2px solid rgba(42, 18, 206, 0.9)";
         firstNameErrorMsg.textContent = "";
         firstNameValue = firstName.value;
-        
+
     } else {
         firstName.style.border = "2px solid red";
         firstNameErrorMsg.textContent = "Le prénom ne doit pas contenir de numéros ou de caractères spéciaux, et avoir une taille comprise entre 2 et 90 caractères.";
-        firstName = false;
+        firstNameValue = false;
     }
     console.log(firstNameValue);
 };
@@ -414,7 +439,7 @@ function checkLastName() {
         lastName.style.border = "2px solid rgba(42, 18, 206, 0.9)";
         lastNameErrorMsg.textContent = "";
         lastNameValue = lastName.value;
-        
+
     } else {
         lastName.style.border = "2px solid red";
         lastNameErrorMsg.textContent = "Le nom ne doit pas contenir de numéros ou de caractères spéciaux, et avoir une taille comprise entre 2 et 90 caractères.";
@@ -428,7 +453,7 @@ function checkAddress() {
         address.style.border = "2px solid rgba(42, 18, 206, 0.9)";
         addressErrorMsg.textContent = "";
         addressValue = address.value;
-        
+
     } else {
         address.style.border = "2px solid red";
         addressErrorMsg.textContent = "L'adresse ne doit pas contenir de caractères spéciaux et avoir une taille comprise entre 2 et 125 caractères.";
@@ -442,7 +467,7 @@ function checkCity() {
         city.style.border = "2px solid rgba(42, 18, 206, 0.9)";
         cityErrorMsg.textContent = "";
         cityValue = city.value;
-        
+
     } else {
         city.style.border = "2px solid red";
         cityErrorMsg.textContent = "La ville ne doit pas contenir de numéros et de caractères spéciaux, et avoir une taille comprise entre 2 et 90 caractères.";
@@ -456,14 +481,15 @@ function checkEmail() {
         email.style.border = "2px solid rgba(42, 18, 206, 0.9)";
         emailErrorMsg.textContent = "";
         emailValue = email.value;
-        
+
     } else {
         email.style.border = "2px solid red";
         emailErrorMsg.textContent = "Le champ renseigné ne correspond pas au format valide d'une adresse email : texte@nomdomaine.txt !";
-        emailValue = false;        
+        emailValue = false;
     }
     console.log(emailValue);
 };
+
 
 /** 
  * Create a personnalized constructor for the client that contains its personnal info
@@ -479,49 +505,26 @@ function Client() {
 
 
 /** 
- * Get product ID in an array to send //OK
+ * Get product ID in an array to send 
  *
  */
 function getOrderedProductId() {
 
-    let productIdArray = [];
+    let productsIdArray = [];
 
+    //Create a loop to get each product id of the products in cart and push them in an array
     for (let product of cart) {
         let productId = product.id;
-        productIdArray.push(productId);
+        productsIdArray.push(productId);
     }
-
-    console.log(productIdArray);
-    return productIdArray;
+    
+    return productsIdArray;
 }
 
 
-/** 
- * Post Order
- *
- */
-
-
-/** 
- * Confirm order
- *
- */
-//  confirmOrder()
-//  function confirmOrder() {
-//     //Select Button
-//     let orderBtn = document.querySelector("#order");
-//     console.log(orderBtn)
-
-//     //Change Button text
-//     orderBtn.textContent = "Commande validée (merci de patienter) !";
-
-//     //Set timer to return to previous state
-//     setTimeout(function(){orderBtn.textContent = "Commander !";},2000)
-
-// }
-
 //Add an event on form to submit the order
-orderForm.addEventListener("submit", (event)=> {
+orderForm.addEventListener("submit", (event) => {
+
     event.preventDefault();
 
     //Run all the checks on input
@@ -531,19 +534,35 @@ orderForm.addEventListener("submit", (event)=> {
     checkCity();
     checkEmail();
 
+    //If any input has a false value, then it will be circled in red thanks to the checkfunction and an alert message will be displayed
     if ((emailValue == false) || (cityValue == false) || (lastNameValue == false) || (firstNameValue == false) || (addressValue == false)) {
         alert("Merci de vérifier et corriger votre formulaire de commande !")
+
     }
 
+    //If all inputs are valid, the order will continue to be processed
     else {
-        let clientOrder = new Client;
-        getOrderedProductId();
-        console.log(clientOrder);
-        //posttheorder
-        //deletekeyinlocalstorage
-        
+        //Create an objet name contact using the client constructor
+        let contact = new Client;
+
+        //Get the array of products ID
+        let products = getOrderedProductId();
+
+        //Create a new object conataining the contact and the products
+        let data = {
+            contact,
+            products,
+        };
+
+        //Post this data object to API and redirect client to confirmation page
+        console.log(data);
+        postOrder(data);
+
+        //Clear the local Storage
+        localStorage.removeItem("cart");
+
     }
 
-})
+});
 
 
